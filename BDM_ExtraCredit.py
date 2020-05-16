51d77404078f74c6bb4d3a7cccb6a2780e5b8701
Mocha22ol <jd3036@nyu.edu>
2020-05-16 14:15:02 -0400

    Update BDM_ExtraCredit.py

--- BDM_ExtraCredit.py
+++ BDM_ExtraCredit.py
@@ -73,12 +73,13 @@ if __name__=='__main__':
     print("***START***")
     print(current_time)
     print("compiling tweets")
-    tweets = sc.textFile('hdfs:///tmp/bdm/tweets-100m.csv')
-    result = tweets.mapPartitionsWithIndex(processTweets)\
-            .reduceByKey(lambda x,y: x+y)
+    #tweets = sc.textFile('hdfs:///tmp/bdm/tweets-100m.csv')
+    #result = tweets.mapPartitionsWithIndex(processTweets)\
+    #        .reduceByKey(lambda x,y: x+y)
     #result = spark.createDataFrame(result, ('tractID','tweets'))
     #print("start base structure")
-    #tract, pop = getTracts("hdfs:///tmp/bdm/500cities_tracts.geojson")
+    df = sqlContext.read.load('500cities_tracts.geojson', format="json")
+    df.printSchema()
     #base_df = spark.createDataFrame(zip(tract, pop), schema=['tract', 'pop'])
     #print("test")
     #result_new = base_df.join(result, base_df.tract == result.tractID, "left").drop('tractID')\
